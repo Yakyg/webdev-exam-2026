feb2f18191b00a898a84bff95ec9168613e4c768
Yakyg <mnych09@mail.ru>
2026-01-13 19:08:55 +0300

    поправил ссылку на API

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ const COURSES_PER_PAGE = 5;
 
 
 
-const API_URL = 'http://exam-api-courses.std-900.ist.mospolytech.ru';
+const API_URL = 'http://exam-api-courses.std-900.ist.mospolytech.ru/';
 const API_KEY = '7f66c6a6-569e-4bb3-945b-b82b7a90c9ac';
 
 function loadCourses() {
